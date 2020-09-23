0dac373afe72cef45eabc4eab1c2eb4dd781c8f3
Yağmur Yıldız <46970065+yagmurx@users.noreply.github.com>
2020-09-23 23:25:47 +0300

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+/* Enter your name
+print "hi, name"
+
+August 2020
+*/
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    string name = get_string("Hi! What's your name? \n");
+    printf("Hi, %s \n", name);
+}

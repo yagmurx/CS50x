8fbfc179dde912b8530ed75970cc13568379a30e
Yağmur Yıldız <46970065+yagmurx@users.noreply.github.com>
2020-09-23 23:31:03 +0300

    Create maribasic.c

---
+++ maribasic.c
@@ -0,0 +1,30 @@
+/*
+Stanford's CS50x Pset1 Task2
+Printing a tower like Mario Game
+September 2020
+*/
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int h;
+    do
+    {
+         h = get_int("Height: ");
+    }
+    while(h<1 || h>8);
+
+    for(int i=1; i<=h; i++)
+    {
+        for(int j=1; j<=h-i; j++)
+        {
+            printf(" ");
+        }
+        for(int k=1; k<=i; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
